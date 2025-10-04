6ef7bbe90aab9a9dcdd1e221333f192701e813ea
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2025-10-04 16:36:27 -0400

    Reorganize BottomRightInfoPanel

--- DebugMod/InfoPanels/BottomRightInfoPanel.cs
+++ DebugMod/InfoPanels/BottomRightInfoPanel.cs
@@ -25,13 +25,13 @@ namespace DebugMod.InfoPanels
                     1f,
                     1f));
 
-            panel.AddText("Right1 Label", "Session Time\nLoad\nHero Pos\nMove Raw", new Vector2(1285, 747), Vector2.zero, GUIController.Instance.arial);
+            panel.AddText("Right1 Label", "Load\nHero Pos\nMove Raw", new Vector2(1285, 747), Vector2.zero, GUIController.Instance.arial);
             panel.AddText("Right1", "", new Vector2(1385, 747), Vector2.zero, GUIController.Instance.trajanNormal);
 
             panel.AddText("Right2 Label", "Move Vector\nKey Pressed\nMove Pressed", new Vector2(1525, 747), Vector2.zero, GUIController.Instance.arial);
             panel.AddText("Right2", "", new Vector2(1625, 747), Vector2.zero, GUIController.Instance.trajanNormal);
 
-            panel.AddText("Right3 Label", "Frame Counter\nGame Time Elapsed", new Vector2(1670, 747), Vector2.zero, GUIController.Instance.arial);
+            panel.AddText("Right3 Label", "Frame Counter\nGame Time Elapsed\nSession Time", new Vector2(1670, 747), Vector2.zero, GUIController.Instance.arial);
             panel.AddText("Right3", "", new Vector2(1800, 747), Vector2.zero, GUIController.Instance.trajanNormal);
 
 
@@ -46,9 +46,9 @@ namespace DebugMod.InfoPanels
             int time1 = Mathf.FloorToInt(Time.realtimeSinceStartup / 60f);
             int time2 = Mathf.FloorToInt(Time.realtimeSinceStartup - (float)(time1 * 60));
 
-            panel.GetText("Right1").UpdateText(string.Format("{0:00}:{1:00}", time1, time2) + "\n" + DebugMod.GetLoadTime() + "s\n" + InfoPanel.GetHeroPos() + "\n" + string.Format("L: {0} R: {1} U: {2} D: {3}", DebugMod.IH.inputActions.Left.RawValue, DebugMod.IH.inputActions.Right.RawValue, DebugMod.IH.inputActions.Up.RawValue, DebugMod.IH.inputActions.Down.RawValue));
+            panel.GetText("Right1").UpdateText(DebugMod.GetLoadTime() + "s\n" + InfoPanel.GetHeroPos() + "\n" + string.Format("L: {0} R: {1} U: {2} D: {3}", DebugMod.IH.inputActions.Left.RawValue, DebugMod.IH.inputActions.Right.RawValue, DebugMod.IH.inputActions.Up.RawValue, DebugMod.IH.inputActions.Down.RawValue));
             panel.GetText("Right2").UpdateText(DebugMod.IH.inputActions.MoveVector.Vector.x + ", " + DebugMod.IH.inputActions.MoveVector.Vector.y + "\n" + InfoPanel.GetStringForBool(InputManager.AnyKeyIsPressed) + "\n" + InfoPanel.GetStringForBool(DebugMod.IH.inputActions.Left.IsPressed || DebugMod.IH.inputActions.Right.IsPressed));
-            panel.GetText("Right3").UpdateText(BindableFunctions.frameCounter + "\n" + frameTime.ToString("mm':'ss'.'ff"));
+            panel.GetText("Right3").UpdateText(BindableFunctions.frameCounter + "\n" + frameTime.ToString("mm':'ss'.'ff") + "\n" + string.Format("{0:00}:{1:00}", time1, time2));
         }
 
         public override bool Active => true;
