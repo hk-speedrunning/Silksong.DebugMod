f995d93a4afab11174ecf07af9d83e434bfb0916
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2025-10-05 19:10:35 -0400

    Set consumable amounts instead of adding

--- DebugMod/BindableFunctions/Consumables.cs
+++ DebugMod/BindableFunctions/Consumables.cs
@@ -16,18 +16,23 @@
             Console.AddLine("Giving player 100 shell shards");
         }
 
+        private static void SetCollectable(string name, int amount)
+        {
+            CollectableItemManager.Instance.AffectItemData(name, (ref CollectableItemsData.Data data) => data.Amount = amount);
+        }
+
         [BindableMethod(name = "Give Memory Lockets", category = "Consumables")]
         public static void GiveMemoryLockets()
         {
-            CollectableItemManager.GetItemByName("Crest Socket Unlocker").AddAmount(10);
-            Console.AddLine("Giving player 10 memory lockets");
+            SetCollectable("Crest Socket Unlocker", 20);
+            Console.AddLine("Set player memory lockets to 20");
         }
 
         [BindableMethod(name = "Give Craftmetal", category = "Consumables")]
         public static void GiveCraftmetal()
         {
-            CollectableItemManager.GetItemByName("Tool Metal").AddAmount(8);
-            Console.AddLine("Giving player 8 craftmetal");
+            SetCollectable("Tool Metal", 8);
+            Console.AddLine("Set player craftmetal to 8");
         }
 
         [BindableMethod(name = "Give Silkeater", category = "Consumables")]
