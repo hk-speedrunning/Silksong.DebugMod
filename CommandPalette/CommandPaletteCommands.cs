af5959c7d4ff1162923fe8b57d297c5d9f0fbfc2
Jakob Hellermann <jakob.hellermann@protonmail.com>
2026-09-06 15:42:26 +0200

    Register savestates in command palette

--- CommandPalette/CommandPaletteCommands.cs
+++ CommandPalette/CommandPaletteCommands.cs
@@ -1,4 +1,6 @@
 using DebugMod.Helpers;
+using DebugMod.SaveStates;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DebugMod.CommandPalette;
@@ -16,7 +18,21 @@ public static class CommandPaletteCommands
                 () => category.Select(action => new CommandPaletteItem.ActionItem(Localization.Get(action.Name), action.Action))
             );
         }
+        
+        registry.RegisterSubmenu("Savestate files", FileSavestates);
 
         return registry;
     }
+    
+    #region Savestates
+
+    private static IEnumerable<CommandPaletteItem> FileSavestates()
+    {
+        foreach (SaveState state in SaveStateManager.AllSavestates)
+        {
+            yield return new CommandPaletteItem.ActionItem(state.ToString(), () => SaveStateManager.LoadState(state));
+        }
+    }
+    
+    #endregion
 }
