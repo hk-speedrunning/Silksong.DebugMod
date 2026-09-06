e2ea2d652bba5033c8753fcf06515fcf4a552fe6
Jakob Hellermann <jakob.hellermann@protonmail.com>
2026-09-06 15:36:43 +0200

    Register bindings in command palette

--- CommandPalette/CommandPaletteCommands.cs
+++ CommandPalette/CommandPaletteCommands.cs
@@ -1,3 +1,6 @@
+using DebugMod.Helpers;
+using System.Linq;
+
 namespace DebugMod.CommandPalette;
 
 public static class CommandPaletteCommands
@@ -5,6 +8,14 @@ public static class CommandPaletteCommands
     public static CommandPaletteRegistry CreateRegistry()
     {
         CommandPaletteRegistry registry = new();
+        
+        foreach (var category in DebugMod.bindActions.Values.GroupBy(action => action.Category))
+        {
+            registry.RegisterSubmenu(
+                Localization.Get(category.Key),
+                () => category.Select(action => new CommandPaletteItem.ActionItem(Localization.Get(action.Name), action.Action))
+            );
+        }
 
         return registry;
     }
