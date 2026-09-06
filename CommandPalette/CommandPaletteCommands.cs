01e165b7f408e1d8ccfb712355b808be57d414ce
Jakob Hellermann <jakob.hellermann@protonmail.com>
2026-09-06 16:03:08 +0200

    Add transition points as teleports to palette

--- CommandPalette/CommandPaletteCommands.cs
+++ CommandPalette/CommandPaletteCommands.cs
@@ -1,12 +1,18 @@
 using DebugMod.Helpers;
 using DebugMod.SaveStates;
+using GlobalEnums;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace DebugMod.CommandPalette;
 
 public static class CommandPaletteCommands
 {
+    private static readonly Dictionary<string, List<string>> transitionPoints = LoadTransitionPoints();
+
     public static CommandPaletteRegistry CreateRegistry()
     {
         CommandPaletteRegistry registry = new();
@@ -20,6 +26,7 @@ public static class CommandPaletteCommands
         }
         
         registry.RegisterSubmenu("Savestate files", FileSavestates);
+        registry.RegisterSubmenu("Teleport", TeleportPoints);
 
         return registry;
     }
@@ -34,5 +41,33 @@ public static class CommandPaletteCommands
         }
     }
     
+    #endregion
+    
+    #region Teleport
+
+    private static IEnumerable<CommandPaletteItem> TeleportPoints()
+        => transitionPoints.Select(scene => new CommandPaletteItem.SubmenuItem(
+            scene.Key,
+            () => scene.Value.Select(gate => new CommandPaletteItem.ActionItem( gate, () => Teleport(scene.Key, gate))),
+            searchChildren: false)
+        );
+
+    private static void Teleport(string scene, string gate)
+    {
+        GameManager.instance.BeginSceneTransition(new GameManager.SceneLoadInfo
+        {
+            SceneName = scene,
+            EntryGateName = gate,
+        });
+    }
+
+    private static Dictionary<string, List<string>> LoadTransitionPoints()
+    {
+        using Stream stream = Assembly.GetExecutingAssembly()
+            .GetManifestResourceStream("DebugMod.CommandPalette.TransitionPoints.json")!;
+        using StreamReader reader = new(stream);
+        return JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(reader.ReadToEnd());
+    }
+
     #endregion
 }
