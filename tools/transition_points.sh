1865b629bdcc06261af13964aa1fdf82a69b3ab4
Jakob Hellermann <hellermann@sipgate.de>
2026-09-07 09:02:43 +0200

    Revert "Add transition points as teleports to palette"
    This reverts commit 01e165b7f408e1d8ccfb712355b808be57d414ce.

--- tools/transition_points.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-# https://github.com/jakobhellermann/rabex-cli
-rabex --steam-game 'Hollow Knight: Silksong' script TransitionPoint references --format json \
-  | jq -c 'reduce (.referrers[] | { scene, label: (.label | sub("@TransitionPoint"; "") | split("/") | last) }) as $point
-      ({}; .[$point.scene] += [$point.label])
-      | with_entries(.value |= (sort | unique))' \
-  > CommandPalette/TransitionPoints.json
